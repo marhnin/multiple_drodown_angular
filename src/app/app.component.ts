1b216deeef94dc58c2ba8d4c2a0ded3c925be479
itworlddev2021 <itworlddev2021@gmail.com>
2022-11-14 11:47:29 +0630

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'multiple_dropdown';
+  cars = [
+    { id: 1, name: "BMW Hyundai" },
+    { id: 2, name: "Kia Tata" },
+    { id: 3, name: "Volkswagen Ford" },
+    { id: 4, name: "Renault Audi" },
+    { id: 5, name: "Mercedes Benz Skoda" },
+  ];
+  selected: any;
+  constructor()
+  {
+    this.selected = [{ id: 3, name: "Volkswagen Ford" }];
+  }
 }
